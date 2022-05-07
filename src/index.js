78e2dc2ec079a957cfa72f535f73128eb73aa34b
ysphdnine <104703824+ysphdnine@users.noreply.github.com>
2022-05-07 13:39:49 +0900

    タスクの追加機能（ボタン）

--- src/index.js
+++ src/index.js
@@ -17,8 +17,24 @@ const onClickAdd = () => {
   p.className = "list-name";
   p.innerText = inputText;
 
+  // button（完了）タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button（削除）タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+
   // divタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // liタグの子要素に各要素を設定
   li.appendChild(div);
